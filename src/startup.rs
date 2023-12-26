a6addf7d6dde39316fb399b2933a21e92061a165
Dan Parkes <dan.parkes@withwise.com>
2023-12-26 15:48:32 +0000

    feat: stub subscription confirmation endpoint

--- src/startup.rs
+++ src/startup.rs
@@ -1,6 +1,6 @@
 use crate::configuration::{DatabaseSettings, Settings};
 use crate::email_client::EmailClient;
-use crate::routes::{health_check, subscribe};
+use crate::routes::{confirm, health_check, subscribe};
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
 use sqlx::postgres::PgPoolOptions;
@@ -75,6 +75,7 @@ pub fn run(
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            .route("/subscriptions/confirm", web::get().to(confirm))
             .app_data(db_pool.clone()) // Clone the connection, as a new app is spawned per thread/core by actix
             .app_data(email_client.clone())
             .app_data(base_url.clone())
